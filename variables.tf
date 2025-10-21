fd58b09bbf54ee0d0887fde151e70b484df15543
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-21 18:58:49 -0500

    refactor: update terraform files to support Google Cloud

--- variables.tf
+++ variables.tf
@@ -43,14 +43,3 @@ variable "cluster_name" {
   default     = "product-mindset-dev"
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS access key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret access key"
-  type        = string
-  sensitive   = true
-}
