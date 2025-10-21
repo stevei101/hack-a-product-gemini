bef0717ae885f4ebf1ff4ff9fa05b5f8e9adc371
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-21 18:37:57 -0500

    Remove GCP and AWS workflows, add Terraform backend config
    Deleted deploy.yml and simple-test.yml workflows for GCP and AWS deployments. Added main.tf for Terraform remote backend configuration. Updated terraform.yml to move environment variables into the job definition for improved organization.

---
+++ .github/workflows/main.tf
@@ -0,0 +1,12 @@
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "disposable-org"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "new-tf-test"
+    }
+  }
+}
