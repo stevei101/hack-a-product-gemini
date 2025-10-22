2af12d738c0252dc6e18d805a8f7b871f82c4293
Steven Irvin <stevei101@users.noreply.github.com>
2025-10-22 01:49:38 -0500

    fix: disable deletion protection for GKE cluster
    - Set deletion_protection = false to allow cluster destruction
    - Required for terraform destroy operations

--- gke.tf
+++ gke.tf
@@ -23,6 +23,7 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection      = false # Allow destruction
 
   network    = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.subnet.self_link
